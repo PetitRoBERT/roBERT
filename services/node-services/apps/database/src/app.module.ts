b8ed8e61728df5dfd942d5d97f36f90ee37f3c56
yleprince <yrieix.leprince@gmail.com>
2020-04-29 19:53:31 +0200

    Mongoose connection done Co-authored-by: resanchez <upol.hdvg@gmail.com>

--- services/node-services/apps/database/src/app.module.ts
+++ services/node-services/apps/database/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorController } from './author/author.controller';
 import { AuthorService } from './author/author.service';
 import { AuthorModule } from './author/author.module';
 
+const server = '127.0.0.1:27017';
+const database = 'devpetitrobert';
 @Module({
-  imports: [AuthorModule],
+  imports: [AuthorModule, MongooseModule.forRoot(`mongodb://${server}/${database}`, { useNewUrlParser: true, useUnifiedTopology: true })],
   controllers: [AuthorController],
   providers: [AuthorService],
 })
-export class AppModule {}
+export class AppModule { }
